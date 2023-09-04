9bf55962da728f0fe5b765cb4ce2617cb1f918d4
Nurs2005 <nursultan250605@gmail.com>
2023-09-04 05:52:03 +0300

    git7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css'
 import React from 'react';
 import AddExpenseForm from './components/AddExpenseForm';
 import Budget from './components/Budget';
